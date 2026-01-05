20ddc3c4dda9aee2446f658f5a74c67986b8a6a3
Lorena Lira <naheelee2005@gmail.com>
2026-01-05 17:15:42 -0300

    Fix loop conditions and average calculation in Aula_96

--- codigos_aula/Aula_96.java
+++ codigos_aula/Aula_96.java
@@ -14,18 +14,18 @@ public class Aula_96 {
 	int n = sc.nextInt();
 	Product[] vect = new Product[n];
 	
-	for (int i =0; i<n; i++ ) {
+	for (int i =0; i<vect.length; i++ ) {
 		sc.nextLine();
 		String name = sc.nextLine();
 		double price = sc.nextDouble();
 		vect[i] = new Product(name, price);
 	}
 	double sum = 0.0;
-	for (int i=0; i<n; i++) {
+	for (int i=0; i<vect.length; i++) {
 		sum += vect[i].getPrice();
 	}
 	
-	double avg = sum / n;
+	double avg = sum / vect.length;
 	System.out.printf("Average price = %.2f%n", avg);
 	sc.close();
 }
